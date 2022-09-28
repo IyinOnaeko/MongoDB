0e4f9c6ad6f758078ff6beb924d42ff32b1b1ba3
Onaeko John <jonaeko11@gmail.com>
2022-09-28 15:38:06 +0100

    connect using mongoose

--- app.js
+++ app.js
@@ -1,66 +1,32 @@
 //jshint esversion:6
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
-
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true })
 // Connection URL
-const url = 'mongodb://localhost:27017';
 
-// Database Name
-const dbName = 'fruitsDB';
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String
+})
 
-// Create a new MongoClient
-const client = new MongoClient(url, {useNewUrlParser: true});
 
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-  const db = client.db(dbName);
-
-  // insertDocuments(db, function() {
-    findDocuments(db, function() {
-
-    client.close();
-});
+const fruit = new Fruit({
+  name: "Apple",
+  rating: 6,
+  review: "sweet"
 });
 
-const insertDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Insert some documents
-  collection.insertMany([
-
-    {
-      name : "Apple",
-      score : 6,
-      review : "sweet"
-    },
-    {
-      name : "Orange",
-      score : 8,
-      review : "sour"
-    },
-    {
-      name : "Pineapple",
-      score : 4,
-      review : "kinda sour"
-    }
-
-  ], function(err, result) {
-    if(err) throw err;
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-}
+fruit.save();
 
 
-const findDocuments = function(db, callback) {
+const findDocuments = function (db, callback) {
   // Get the documents collection
   const collection = db.collection('fruits');
   // Find some documents
-  collection.find({}).toArray(function(err, fruits) {
+  collection.find({}).toArray(function (err, fruits) {
     assert.equal(err, null);
     console.log("Found the following records");
     console.log(fruits)
